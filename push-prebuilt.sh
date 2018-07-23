c5b46d4466ea9eddad01f4dc0dfc3d1448af3e24
Fabian Trampusch <fabian.trampusch@freenet.de>
2018-07-23 10:11:00 +0200

    release 0.1.1

--- push-prebuilt.sh
+++ push-prebuilt.sh
@@ -7,12 +7,5 @@ rm -rf dist
 npm run build-lib
 cp README.md dist/pazznetwork/ngx-chat
 cd dist/pazznetwork/ngx-chat
-git init .
-git remote add origin git@github.com:pazznetwork/ngx-chat-xmpp-alpha-prebuilt.git
-git fetch
-git reset --soft origin/master
-git add .
-git commit -m "$MESSAGE ($VERSION)"
-git push origin master
 
 npm publish
