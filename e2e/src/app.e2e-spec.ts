5459766688a1716009321d568472e7026062b1e7
Fabian Trampusch <fabian.trampusch@freenet.de>
2018-11-16 08:53:09 +0100

    test: first failing e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,28 @@
+import { browser } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+    it('should display welcome message', () => {
+        page.navigateTo();
+
+        page.logIn({
+            jid: '',
+            password: '',
+            uri: '',
+            domain: ''
+        });
+
+        browser.sleep(5000);
+
+        page.logOut();
+
+        browser.sleep(5000);
+
+    });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,34 @@
 import { browser, by, element } from 'protractor';
+import { LogInRequest } from '../../projects/pazznetwork/ngx-chat/src/lib/core';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+    private readonly domainInput = element(by.name('domain'));
+    private readonly uriInput = element(by.name('uri'));
+    private readonly jidInput = element(by.name('jid'));
+    private readonly passwordInput = element(by.name('password'));
+    private readonly loginButton = element(by.name('login'));
+    private readonly logoutButton = element(by.name('logout'));
+    private readonly registerButton = element(by.name('register'));
+
+    navigateTo() {
+        return browser.get('/');
+    }
+
+    logIn(logInRequest: LogInRequest) {
+        this.domainInput.clear();
+        this.domainInput.sendKeys(logInRequest.domain);
+        this.uriInput.clear();
+        this.uriInput.sendKeys(logInRequest.uri);
+        this.jidInput.clear();
+        this.jidInput.sendKeys(logInRequest.jid);
+        this.passwordInput.clear();
+        this.passwordInput.sendKeys(logInRequest.password);
+        this.loginButton.click();
+    }
+
+    logOut() {
+        this.logoutButton.click();
+    }
+
 }
