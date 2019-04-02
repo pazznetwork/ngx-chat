db2bdafc3515c1422d5d3d5a324c32e8d4bcedba
Fabian Trampusch <fabian.trampusch@freenet.de>
2019-04-02 09:30:50 +0200

    fix: remove devDependency on @angular/cdk (it is a peer dependency)

--- src/app/ngx-chat-imports.ts
+++ src/app/ngx-chat-imports.ts
@@ -1,3 +1,3 @@
 // this file is just for development purposes to switch fast between the pre bundled and the development version
-// export * from '@pazznetwork/ngx-chat';
-export * from '../../projects/pazznetwork/ngx-chat/src/public_api';
+export * from '@pazznetwork/ngx-chat';
+// export * from '../../projects/pazznetwork/ngx-chat/src/public_api';
