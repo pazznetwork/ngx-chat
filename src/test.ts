d85143b56e45b612b011ee61bfb88f8bf3dec476
Jakub Mikos <20125737+CodeBast4rd@users.noreply.github.com>
2021-11-23 10:45:20 +0100

    refactor: cleaned up the repo for easier integration tests

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,25 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
+/**
+ * IMPORTANT
+ * Keep the imports zone.js first in order, otherwise the karma runner breaks.
+ * Required order is: 'zone.js', 'zone.js/testing' ...
+ */
+import 'zone.js';
+import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js/testing';
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
+    context(path: string, deep?: boolean, filter?: RegExp): {
+        keys(): string[];
+        <T>(id: string): T;
+    };
 };
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
     BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting()
+    platformBrowserDynamicTesting(),
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
