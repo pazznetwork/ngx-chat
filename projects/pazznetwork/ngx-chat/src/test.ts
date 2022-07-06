9e49333280d2c43976fbdab704aef051fe1e764f
CodeBast4rd <20125737+CodeBast4rd@users.noreply.github.com>
2022-07-06 15:54:57 +0200

    feat(angular 13): core and cli

--- projects/pazznetwork/ngx-chat/src/test.ts
+++ projects/pazznetwork/ngx-chat/src/test.ts
@@ -2,13 +2,14 @@
 
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import 'zone.js';
 import 'zone.js/testing';
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
+    context(path: string, deep?: boolean, filter?: RegExp): {
+        keys(): string[];
+        <T>(id: string): T;
+    };
 };
 
 // First, initialize the Angular testing environment.
