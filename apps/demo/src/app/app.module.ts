dd46667f7638d456d3ae61e475d8da9f6e8bbc8e
CodeBast4rd <20125737+CodeBast4rd@users.noreply.github.com>
2023-10-17 10:24:05 +0200

    feature(demo): iq component to stanza

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { IndexComponent } from './routes/index/index.component';
-import { IqComponent } from './iq/iq.component';
+import { StanzaComponent } from './components/stanza/stanza.component';
 import { MucComponent } from './routes/muc/muc.component';
-import { MultiUserChatComponent } from './multi-user-chat/multi-user-chat.component';
+import { MultiUserChatComponent } from './components/multi-user-chat/multi-user-chat.component';
 import { NgModule } from '@angular/core';
 import { NgxChatModule } from '@pazznetwork/ngx-chat';
 import { UiComponent } from './routes/ui/ui.component';
@@ -16,7 +16,7 @@ import { UiComponent } from './routes/ui/ui.component';
   declarations: [
     AppComponent,
     IndexComponent,
-    IqComponent,
+    StanzaComponent,
     MucComponent,
     MultiUserChatComponent,
     UiComponent,
